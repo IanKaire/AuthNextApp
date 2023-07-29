896359d388c71c6e7e64b269ece9a1624658f13b
Ian Kaire <iankaire@gmail.com>
2023-07-29 16:47:33 +0300

    first commit of NextJS authentication app

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
+
+import classes from './main-navigation.module.css';
+
+function MainNavigation() {
+  const { data: session, status } = useSession();
+
+  function logoutHandler() {
+    signOut();
+  }
+
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+        <div className={classes.logo}>Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!session && status === 'unauthenticated' && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import { SessionProvider } from "next-auth/react";
+
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
+
+function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
+}
+
+export default App;
